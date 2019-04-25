edb661228177204d48abbde365c99a3096256c32
Tom Monaghan <105718@student.sae.edu.au>
2019-04-25 23:02:48 +1000

    Reset on button press
    Added when you hit 'J' (will change to enter) the black deletes away showing the level and the game scene restarts.

---
+++ Contrast/Assets/Scenes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    
+    public void hardRestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            hardRestartGame();
+        }
+        
+    }
+}
+
