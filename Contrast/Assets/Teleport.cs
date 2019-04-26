93950e120da828a98a7a1eaa45a5d40e4c9e6f95
Harry <1011422@student.sae.edu.au>
2019-04-26 16:25:29 +1000

    Camera!

---
+++ Contrast/Assets/Teleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject cam;
+
+    public float xRight, xLeft;
+    public float offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x < xLeft)
+        {
+            
+        }
+
+        if (player.transform.position.x > xRight)
+        {
+            
+        }
+    }
+}
