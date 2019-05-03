a4b2416f84e8502717a0f55580344140839539c1
Tom <105718@student.sae.edu.au>
2019-05-03 16:06:01 +1000

    Audio transition now working

---
+++ Contrast/Assets/MusicTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicTrigger : MonoBehaviour
+{
+    public AudioMixerSnapshot MixerSnapshot;
+    public float fadeIntoMusicTime = 0;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            MixerSnapshot.TransitionTo(fadeIntoMusicTime);
+            Debug.Log("MusicTriggerTriggered");
+        }
+    }
+    
+    
+}
